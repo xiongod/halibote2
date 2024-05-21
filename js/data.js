04854b02560be1d5ff96d31cd3446b4ffbbd5d6f
xiongod <xiongod@outlook.com>
2024-05-21 11:42:20 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -5,7 +5,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 49
+        "chapter": 49,
+        "id": "c98b0628878348c1b823102f5810a44a"
     },
     {
         "title": "50.【试听】《哈利·波特》第二部 第1集 最糟糕的生日1.m4a",
@@ -13,7 +14,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 50
+        "chapter": 50,
+        "id": "f851f49389884211a5a6f4a966ef19b5"
     },
     {
         "title": "51.【试听】《哈利·波特》第二部 第2集 最糟糕的生日2.m4a",
@@ -21,7 +23,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 51
+        "chapter": 51,
+        "id": "32ab3de5835042e19f8e33a8cb7f35ae"
     },
     {
         "title": "52.《哈利·波特》第二部 第3集 多比的警告1.m4a",
@@ -29,7 +32,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 52
+        "chapter": 52,
+        "id": "7343d725cee54664b88dc9e0b59e9598"
     },
     {
         "title": "53.《哈利·波特》第二部 第4集 多比的警告2.m4a",
@@ -37,7 +41,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 53
+        "chapter": 53,
+        "id": "9d6780c58bb442d5877ed40374021ae5"
     },
     {
         "title": "54.《哈利·波特》第二部 第5集 陋居1.m4a",
@@ -45,7 +50,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 54
+        "chapter": 54,
+        "id": "c56775a17a4f49d580c6f09065aebfc1"
     },
     {
         "title": "55.《哈利·波特》第二部 第6集 陋居2.m4a",
@@ -53,7 +59,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 55
+        "chapter": 55,
+        "id": "0ee02efc4bef47309ca07f6149adc0d0"
     },
     {
         "title": "56.《哈利·波特》第二部 第7集 陋居3.m4a",
@@ -61,7 +68,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 56
+        "chapter": 56,
+        "id": "0106fa24391848b8a4dc50896210054b"
     },
     {
         "title": "57.《哈利·波特》第二部 第8集 在丽痕书店1.m4a",
@@ -69,7 +77,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 57
+        "chapter": 57,
+        "id": "dd1996e0ba2e4ea5af2ad126ed2203c4"
     },
     {
         "title": "58.《哈利·波特》第二部 第9集 在丽痕书店2.m4a",
@@ -77,7 +86,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 58
+        "chapter": 58,
+        "id": "8f35935bb0fb48d3a842af57fe7beff7"
     },
     {
         "title": "59.《哈利·波特》第二部 第10集 在丽痕书店3.m4a",
@@ -85,7 +95,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 59
+        "chapter": 59,
+        "id": "747db35fc52c471cb669ca6f2366a930"
     },
     {
         "title": "60.《哈利·波特》第二部 第11集 打人柳1.m4a",
@@ -93,7 +104,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 60
+        "chapter": 60,
+        "id": "d24a1264bbd14a53a50f31bc38fd350f"
     },
     {
         "title": "61.《哈利·波特》第二部 第12集 打人柳2.m4a",
@@ -101,7 +113,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 61
+        "chapter": 61,
+        "id": "065577c75b1247a3a6a1ca86454aeec4"
     },
     {
         "title": "62.《哈利·波特》第二部 第13集 打人柳3.m4a",
@@ -109,7 +122,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 62
+        "chapter": 62,
+        "id": "ad7869aa7da94a1a91b9d3c7e8d9d182"
     },
     {
         "title": "63.《哈利·波特》第二部 第14集 吉德罗·洛哈特1.m4a",
@@ -117,7 +131,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 63
+        "chapter": 63,
+        "id": "36409e5b4c364563a4bab6f00899259d"
     },
     {
         "title": "64.《哈利·波特》第二部 第15集 吉德罗·洛哈特2.m4a",
@@ -125,7 +140,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 64
+        "chapter": 64,
+        "id": "e727d939ab6e4956b7e7e3da6e93a92a"
     },
     {
         "title": "65.《哈利·波特》第二部 第16集 泥巴种和细语1.m4a",
@@ -133,7 +149,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 65
+        "chapter": 65,
+        "id": "f2348a2c6f654d2bb500601c8e344af4"
     },
     {
         "title": "66.《哈利·波特》第二部 第17集 泥巴种和细语2.m4a",
@@ -141,7 +158,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 66
+        "chapter": 66,
+        "id": "f547efa50ab04d278319d7bbf323ee45"
     },
     {
         "title": "67.《哈利·波特》第二部 第18集 泥巴种和细语3.m4a",
@@ -149,7 +167,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 67
+        "chapter": 67,
+        "id": "1e2d4cbd5fa24de387ba951db0035928"
     },
     {
         "title": "68.《哈利·波特》第二部 第19集 忌辰晚会1.m4a",
@@ -157,7 +176,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 68
+        "chapter": 68,
+        "id": "10541946a5ec4317a66a9d42c28ee781"
     },
     {
         "title": "69.《哈利·波特》第二部 第20集 忌辰晚会2.m4a",
@@ -165,7 +185,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 69
+        "chapter": 69,
+        "id": "c039abf2712943ed854e7518f77de966"
     },
     {
         "title": "70.《哈利·波特》第二部 第21集 忌辰晚会3.m4a",
@@ -173,7 +194,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 70
+        "chapter": 70,
+        "id": "aa620228472c4739868aef8cf7b4cf9f"
     },
     {
         "title": "71.《哈利·波特》第二部 第22集 墙上的字1.m4a",
@@ -181,7 +203,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 71
+        "chapter": 71,
+        "id": "bdde3f250fdc44319c89cc65224a0050"
     },
     {
         "title": "72.《哈利·波特》第二部 第23集 墙上的字2.m4a",
@@ -189,7 +212,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 72
+        "chapter": 72,
+        "id": "74a9a0eb6bc947ae926fe51fd2cfe45d"
     },
     {
         "title": "73.《哈利·波特》第二部 第24集 墙上的字3.m4a",
@@ -197,7 +221,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 73
+        "chapter": 73,
+        "id": "cc179d2fd8084eb3b58cfe8decf6bf35"
     },
     {
         "title": "74.《哈利·波特》第二部 第25集 失控的游走球1.m4a",
@@ -205,7 +230,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 74
+        "chapter": 74,
+        "id": "a888d69550aa4e5c9dd8996e1aaa4fbb"
     },
     {
         "title": "75.《哈利·波特》第二部 第26集 失控的游走球2.m4a",
@@ -213,7 +239,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 75
+        "chapter": 75,
+        "id": "66d826f6362e4afcae0a1a482fabf29d"
     },
     {
         "title": "76.《哈利·波特》第二部 第27集 失控的游走球3.m4a",
@@ -221,7 +248,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 76
+        "chapter": 76,
+        "id": "828c7e113cae498abf86d14c0266522f"
     },
     {
         "title": "77.《哈利·波特》第二部 第28集 失控的游走球4.m4a",
@@ -229,7 +257,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 77
+        "chapter": 77,
+        "id": "9b428197f86a47ccb421312b75658366"
     },
     {
         "title": "78.《哈利·波特》第二部 第29集 决斗俱乐部1.m4a",
@@ -237,7 +266,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 78
+        "chapter": 78,
+        "id": "72f4018a19b549ffb1917469e6bcd340"
     },
     {
         "title": "79.《哈利·波特》第二部 第30集 决斗俱乐部2.m4a",
@@ -245,7 +275,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 79
+        "chapter": 79,
+        "id": "8680f6a6b33143fcaeb78119d90cc950"
     },
     {
         "title": "80.《哈利·波特》第二部 第31集 决斗俱乐部3.m4a",
@@ -253,7 +284,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 80
+        "chapter": 80,
+        "id": "e25a96cffc78442e8f64756893401cbb"
     },
     {
         "title": "81.《哈利·波特》第二部 第32集 决斗俱乐部4.m4a",
@@ -261,7 +293,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 81
+        "chapter": 81,
+        "id": "854b19fa26d748c89efc232f8942bcf7"
     },
     {
         "title": "82.《哈利·波特》第二部 第33集 复方汤剂1.m4a",
@@ -269,7 +302,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 82
+        "chapter": 82,
+        "id": "26d54d35409e4831b245185751bce4da"
     },
     {
         "title": "83.《哈利·波特》第二部 第34集 复方汤剂2.m4a",
@@ -277,7 +311,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 83
+        "chapter": 83,
+        "id": "40d8a09b3b4f4d018d9838a3a9d939fe"
     },
     {
         "title": "84.《哈利·波特》第二部 第35集 复方汤剂3.m4a",
@@ -285,7 +320,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 84
+        "chapter": 84,
+        "id": "85afdb1ae63c407b85f03f5a25847847"
     },
     {
         "title": "85.《哈利·波特》第二部 第36集 复方汤剂4.m4a",
@@ -293,7 +329,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 85
+        "chapter": 85,
+        "id": "f71aee408b5448f09b31b3ed86a8a492"
     },
     {
         "title": "86.《哈利·波特》第二部 第37集 绝密日记1.m4a",
@@ -301,7 +338,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 86
+        "chapter": 86,
+        "id": "a0044ef3d5604cc99d71f570d186af88"
     },
     {
         "title": "87.《哈利·波特》第二部 第38集 绝密日记2.m4a",
@@ -309,7 +347,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 87
+        "chapter": 87,
+        "id": "15071dd7d7dc48cc960736a12a311f6a"
     },
     {
         "title": "88.《哈利·波特》第二部 第39集 绝密日记3.m4a",
@@ -317,7 +356,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 88
+        "chapter": 88,
+        "id": "4b43056262ac4c2dae16a9247c49d062"
     },
     {
         "title": "89.《哈利·波特》第二部 第40集 康奈利·福吉1.m4a",
@@ -325,7 +365,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 89
+        "chapter": 89,
+        "id": "e3e7f3b53f88407697f0e40f7fc30067"
     },
     {
         "title": "90.《哈利·波特》第二部 第41集 康奈利·福吉2.m4a",
@@ -333,7 +374,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 90
+        "chapter": 90,
+        "id": "0c855963c8c94401884ddf3113e502b0"
     },
     {
         "title": "91.《哈利·波特》第二部 第42集 康奈利·福吉3.m4a",
@@ -341,7 +383,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 91
+        "chapter": 91,
+        "id": "5fc632637ad94a2cb4a67f6a2a53c3ac"
     },
     {
         "title": "92.《哈利·波特》第二部 第43集 阿拉戈克1.m4a",
@@ -349,7 +392,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 92
+        "chapter": 92,
+        "id": "6038db191b9e4637a0dfb26f93e145bb"
     },
     {
         "title": "93.《哈利·波特》第二部 第44集 阿拉戈克2.m4a",
@@ -357,7 +401,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 93
+        "chapter": 93,
+        "id": "bd617490cb3149189f0d6443fc08d009"
     },
     {
         "title": "94.《哈利·波特》第二部 第45集 阿拉戈克3.m4a",
@@ -365,7 +410,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 94
+        "chapter": 94,
+        "id": "52c06b9e2d20465f915846269c984e55"
     },
     {
         "title": "95.《哈利·波特》第二部 第46集 密室1.m4a",
@@ -373,7 +419,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 95
+        "chapter": 95,
+        "id": "d2be2ede22384009a6a48a44edc2dd89"
     },
     {
         "title": "96.《哈利·波特》第二部 第47集 密室2.m4a",
@@ -381,7 +428,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 96
+        "chapter": 96,
+        "id": "4736a5003f864d23bf38d07e036eed72"
     },
     {
         "title": "97.《哈利·波特》第二部 第48集 密室3.m4a",
@@ -389,7 +437,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 97
+        "chapter": 97,
+        "id": "a26249e393864e34919b489d2ff1d0b5"
     },
     {
         "title": "98.《哈利·波特》第二部 第49集 密室4.m4a",
@@ -397,7 +446,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 98
+        "chapter": 98,
+        "id": "b5bc713a205743559a64a03ac05dfe79"
     },
     {
         "title": "99.《哈利·波特》第二部 第50集 斯莱特林的继承人1.m4a",
@@ -405,7 +455,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 99
+        "chapter": 99,
+        "id": "91ca329948cb4cd9a964b35f91276750"
     },
     {
         "title": "100.《哈利·波特》第二部 第51集 斯莱特林的继承人2.m4a",
@@ -413,7 +464,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 100
+        "chapter": 100,
+        "id": "6f3c0e121fce4033a3f1ff159f49928d"
     },
     {
         "title": "101.《哈利·波特》第二部 第52集 斯莱特林的继承人3.m4a",
@@ -421,7 +473,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 101
+        "chapter": 101,
+        "id": "cf3b948bd6ab446aaedd8c1c72c945ac"
     },
     {
         "title": "102.《哈利·波特》第二部 第53集 斯莱特林的继承人4.m4a",
@@ -429,7 +482,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 102
+        "chapter": 102,
+        "id": "6003a586ade84fa4822af29bcc868708"
     },
     {
         "title": "103.《哈利·波特》第二部 第54集 多比的报偿1.m4a",
@@ -437,7 +491,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 103
+        "chapter": 103,
+        "id": "995e22b693ed432bb193b4d215a6e703"
     },
     {
         "title": "104.《哈利·波特》第二部 第55集 多比的报偿2.m4a",
@@ -445,7 +500,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 104
+        "chapter": 104,
+        "id": "8074617c6f5d461186d01f9a6202baae"
     },
     {
         "title": "105.《哈利·波特》第二部 第56集 多比的报偿3.m4a",
@@ -453,6 +509,7 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 105
+        "chapter": 105,
+        "id": "de2db6e4e5a44d438eac4259cef4774e"
     }
 ]
