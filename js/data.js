b0bbd5439362d07ac5332401b0733dcfa91371a4
xiongod <xiongod@outlook.com>
2024-05-28 17:12:09 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 [
     {
-        "title": "49.【预告】我们邀您一起听见魔法的力量！.m4a",
+        "title": "【预告】我们邀您一起听见魔法的力量！.ma",
         "url": "https://xiongod.github.io/halibote2/049.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -9,7 +9,7 @@
         "id": "c98b0628878348c1b823102f5810a44a"
     },
     {
-        "title": "50.【试听】《哈利·波特》第二部 第1集 最糟糕的生日1.m4a",
+        "title": "【试听】《哈利·波特》第二部 第集 最糟糕的生日.ma",
         "url": "https://xiongod.github.io/halibote2/050.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -18,7 +18,7 @@
         "id": "f851f49389884211a5a6f4a966ef19b5"
     },
     {
-        "title": "51.【试听】《哈利·波特》第二部 第2集 最糟糕的生日2.m4a",
+        "title": "【试听】《哈利·波特》第二部 第集 最糟糕的生日.ma",
         "url": "https://xiongod.github.io/halibote2/051.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -27,7 +27,7 @@
         "id": "32ab3de5835042e19f8e33a8cb7f35ae"
     },
     {
-        "title": "52.《哈利·波特》第二部 第3集 多比的警告1.m4a",
+        "title": "《哈利·波特》第二部 第集 多比的警告.ma",
         "url": "https://xiongod.github.io/halibote2/052.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -36,7 +36,7 @@
         "id": "7343d725cee54664b88dc9e0b59e9598"
     },
     {
-        "title": "53.《哈利·波特》第二部 第4集 多比的警告2.m4a",
+        "title": "《哈利·波特》第二部 第集 多比的警告.ma",
         "url": "https://xiongod.github.io/halibote2/053.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -45,7 +45,7 @@
         "id": "9d6780c58bb442d5877ed40374021ae5"
     },
     {
-        "title": "54.《哈利·波特》第二部 第5集 陋居1.m4a",
+        "title": "《哈利·波特》第二部 第集 陋居.ma",
         "url": "https://xiongod.github.io/halibote2/054.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -54,7 +54,7 @@
         "id": "c56775a17a4f49d580c6f09065aebfc1"
     },
     {
-        "title": "55.《哈利·波特》第二部 第6集 陋居2.m4a",
+        "title": "《哈利·波特》第二部 第集 陋居.ma",
         "url": "https://xiongod.github.io/halibote2/055.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -63,7 +63,7 @@
         "id": "0ee02efc4bef47309ca07f6149adc0d0"
     },
     {
-        "title": "56.《哈利·波特》第二部 第7集 陋居3.m4a",
+        "title": "《哈利·波特》第二部 第集 陋居.ma",
         "url": "https://xiongod.github.io/halibote2/056.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -72,7 +72,7 @@
         "id": "0106fa24391848b8a4dc50896210054b"
     },
     {
-        "title": "57.《哈利·波特》第二部 第8集 在丽痕书店1.m4a",
+        "title": "《哈利·波特》第二部 第集 在丽痕书店.ma",
         "url": "https://xiongod.github.io/halibote2/057.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -81,7 +81,7 @@
         "id": "dd1996e0ba2e4ea5af2ad126ed2203c4"
     },
     {
-        "title": "58.《哈利·波特》第二部 第9集 在丽痕书店2.m4a",
+        "title": "《哈利·波特》第二部 第集 在丽痕书店.ma",
         "url": "https://xiongod.github.io/halibote2/058.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -90,7 +90,7 @@
         "id": "8f35935bb0fb48d3a842af57fe7beff7"
     },
     {
-        "title": "59.《哈利·波特》第二部 第10集 在丽痕书店3.m4a",
+        "title": "《哈利·波特》第二部 第集 在丽痕书店.ma",
         "url": "https://xiongod.github.io/halibote2/059.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -99,7 +99,7 @@
         "id": "747db35fc52c471cb669ca6f2366a930"
     },
     {
-        "title": "60.《哈利·波特》第二部 第11集 打人柳1.m4a",
+        "title": "《哈利·波特》第二部 第集 打人柳.ma",
         "url": "https://xiongod.github.io/halibote2/060.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -108,7 +108,7 @@
         "id": "d24a1264bbd14a53a50f31bc38fd350f"
     },
     {
-        "title": "61.《哈利·波特》第二部 第12集 打人柳2.m4a",
+        "title": "《哈利·波特》第二部 第集 打人柳.ma",
         "url": "https://xiongod.github.io/halibote2/061.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -117,7 +117,7 @@
         "id": "065577c75b1247a3a6a1ca86454aeec4"
     },
     {
-        "title": "62.《哈利·波特》第二部 第13集 打人柳3.m4a",
+        "title": "《哈利·波特》第二部 第集 打人柳.ma",
         "url": "https://xiongod.github.io/halibote2/062.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -126,7 +126,7 @@
         "id": "ad7869aa7da94a1a91b9d3c7e8d9d182"
     },
     {
-        "title": "63.《哈利·波特》第二部 第14集 吉德罗·洛哈特1.m4a",
+        "title": "《哈利·波特》第二部 第集 吉德罗·洛哈特.ma",
         "url": "https://xiongod.github.io/halibote2/063.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -135,7 +135,7 @@
         "id": "36409e5b4c364563a4bab6f00899259d"
     },
     {
-        "title": "64.《哈利·波特》第二部 第15集 吉德罗·洛哈特2.m4a",
+        "title": "《哈利·波特》第二部 第集 吉德罗·洛哈特.ma",
         "url": "https://xiongod.github.io/halibote2/064.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -144,7 +144,7 @@
         "id": "e727d939ab6e4956b7e7e3da6e93a92a"
     },
     {
-        "title": "65.《哈利·波特》第二部 第16集 泥巴种和细语1.m4a",
+        "title": "《哈利·波特》第二部 第集 泥巴种和细语.ma",
         "url": "https://xiongod.github.io/halibote2/065.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -153,7 +153,7 @@
         "id": "f2348a2c6f654d2bb500601c8e344af4"
     },
     {
-        "title": "66.《哈利·波特》第二部 第17集 泥巴种和细语2.m4a",
+        "title": "《哈利·波特》第二部 第集 泥巴种和细语.ma",
         "url": "https://xiongod.github.io/halibote2/066.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -162,7 +162,7 @@
         "id": "f547efa50ab04d278319d7bbf323ee45"
     },
     {
-        "title": "67.《哈利·波特》第二部 第18集 泥巴种和细语3.m4a",
+        "title": "《哈利·波特》第二部 第集 泥巴种和细语.ma",
         "url": "https://xiongod.github.io/halibote2/067.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -171,7 +171,7 @@
         "id": "1e2d4cbd5fa24de387ba951db0035928"
     },
     {
-        "title": "68.《哈利·波特》第二部 第19集 忌辰晚会1.m4a",
+        "title": "《哈利·波特》第二部 第集 忌辰晚会.ma",
         "url": "https://xiongod.github.io/halibote2/068.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -180,7 +180,7 @@
         "id": "10541946a5ec4317a66a9d42c28ee781"
     },
     {
-        "title": "69.《哈利·波特》第二部 第20集 忌辰晚会2.m4a",
+        "title": "《哈利·波特》第二部 第集 忌辰晚会.ma",
         "url": "https://xiongod.github.io/halibote2/069.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -189,7 +189,7 @@
         "id": "c039abf2712943ed854e7518f77de966"
     },
     {
-        "title": "70.《哈利·波特》第二部 第21集 忌辰晚会3.m4a",
+        "title": "《哈利·波特》第二部 第集 忌辰晚会.ma",
         "url": "https://xiongod.github.io/halibote2/070.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -198,7 +198,7 @@
         "id": "aa620228472c4739868aef8cf7b4cf9f"
     },
     {
-        "title": "71.《哈利·波特》第二部 第22集 墙上的字1.m4a",
+        "title": "《哈利·波特》第二部 第集 墙上的字.ma",
         "url": "https://xiongod.github.io/halibote2/071.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -207,7 +207,7 @@
         "id": "bdde3f250fdc44319c89cc65224a0050"
     },
     {
-        "title": "72.《哈利·波特》第二部 第23集 墙上的字2.m4a",
+        "title": "《哈利·波特》第二部 第集 墙上的字.ma",
         "url": "https://xiongod.github.io/halibote2/072.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -216,7 +216,7 @@
         "id": "74a9a0eb6bc947ae926fe51fd2cfe45d"
     },
     {
-        "title": "73.《哈利·波特》第二部 第24集 墙上的字3.m4a",
+        "title": "《哈利·波特》第二部 第集 墙上的字.ma",
         "url": "https://xiongod.github.io/halibote2/073.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -225,7 +225,7 @@
         "id": "cc179d2fd8084eb3b58cfe8decf6bf35"
     },
     {
-        "title": "74.《哈利·波特》第二部 第25集 失控的游走球1.m4a",
+        "title": "《哈利·波特》第二部 第集 失控的游走球.ma",
         "url": "https://xiongod.github.io/halibote2/074.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -234,7 +234,7 @@
         "id": "a888d69550aa4e5c9dd8996e1aaa4fbb"
     },
     {
-        "title": "75.《哈利·波特》第二部 第26集 失控的游走球2.m4a",
+        "title": "《哈利·波特》第二部 第集 失控的游走球.ma",
         "url": "https://xiongod.github.io/halibote2/075.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -243,7 +243,7 @@
         "id": "66d826f6362e4afcae0a1a482fabf29d"
     },
     {
-        "title": "76.《哈利·波特》第二部 第27集 失控的游走球3.m4a",
+        "title": "《哈利·波特》第二部 第集 失控的游走球.ma",
         "url": "https://xiongod.github.io/halibote2/076.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -252,7 +252,7 @@
         "id": "828c7e113cae498abf86d14c0266522f"
     },
     {
-        "title": "77.《哈利·波特》第二部 第28集 失控的游走球4.m4a",
+        "title": "《哈利·波特》第二部 第集 失控的游走球.ma",
         "url": "https://xiongod.github.io/halibote2/077.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -261,7 +261,7 @@
         "id": "9b428197f86a47ccb421312b75658366"
     },
     {
-        "title": "78.《哈利·波特》第二部 第29集 决斗俱乐部1.m4a",
+        "title": "《哈利·波特》第二部 第集 决斗俱乐部.ma",
         "url": "https://xiongod.github.io/halibote2/078.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -270,7 +270,7 @@
         "id": "72f4018a19b549ffb1917469e6bcd340"
     },
     {
-        "title": "79.《哈利·波特》第二部 第30集 决斗俱乐部2.m4a",
+        "title": "《哈利·波特》第二部 第集 决斗俱乐部.ma",
         "url": "https://xiongod.github.io/halibote2/079.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -279,7 +279,7 @@
         "id": "8680f6a6b33143fcaeb78119d90cc950"
     },
     {
-        "title": "80.《哈利·波特》第二部 第31集 决斗俱乐部3.m4a",
+        "title": "《哈利·波特》第二部 第集 决斗俱乐部.ma",
         "url": "https://xiongod.github.io/halibote2/080.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -288,7 +288,7 @@
         "id": "e25a96cffc78442e8f64756893401cbb"
     },
     {
-        "title": "81.《哈利·波特》第二部 第32集 决斗俱乐部4.m4a",
+        "title": "《哈利·波特》第二部 第集 决斗俱乐部.ma",
         "url": "https://xiongod.github.io/halibote2/081.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -297,7 +297,7 @@
         "id": "854b19fa26d748c89efc232f8942bcf7"
     },
     {
-        "title": "82.《哈利·波特》第二部 第33集 复方汤剂1.m4a",
+        "title": "《哈利·波特》第二部 第集 复方汤剂.ma",
         "url": "https://xiongod.github.io/halibote2/082.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -306,7 +306,7 @@
         "id": "26d54d35409e4831b245185751bce4da"
     },
     {
-        "title": "83.《哈利·波特》第二部 第34集 复方汤剂2.m4a",
+        "title": "《哈利·波特》第二部 第集 复方汤剂.ma",
         "url": "https://xiongod.github.io/halibote2/083.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -315,7 +315,7 @@
         "id": "40d8a09b3b4f4d018d9838a3a9d939fe"
     },
     {
-        "title": "84.《哈利·波特》第二部 第35集 复方汤剂3.m4a",
+        "title": "《哈利·波特》第二部 第集 复方汤剂.ma",
         "url": "https://xiongod.github.io/halibote2/084.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -324,7 +324,7 @@
         "id": "85afdb1ae63c407b85f03f5a25847847"
     },
     {
-        "title": "85.《哈利·波特》第二部 第36集 复方汤剂4.m4a",
+        "title": "《哈利·波特》第二部 第集 复方汤剂.ma",
         "url": "https://xiongod.github.io/halibote2/085.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -333,7 +333,7 @@
         "id": "f71aee408b5448f09b31b3ed86a8a492"
     },
     {
-        "title": "86.《哈利·波特》第二部 第37集 绝密日记1.m4a",
+        "title": "《哈利·波特》第二部 第集 绝密日记.ma",
         "url": "https://xiongod.github.io/halibote2/086.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -342,7 +342,7 @@
         "id": "a0044ef3d5604cc99d71f570d186af88"
     },
     {
-        "title": "87.《哈利·波特》第二部 第38集 绝密日记2.m4a",
+        "title": "《哈利·波特》第二部 第集 绝密日记.ma",
         "url": "https://xiongod.github.io/halibote2/087.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -351,7 +351,7 @@
         "id": "15071dd7d7dc48cc960736a12a311f6a"
     },
     {
-        "title": "88.《哈利·波特》第二部 第39集 绝密日记3.m4a",
+        "title": "《哈利·波特》第二部 第集 绝密日记.ma",
         "url": "https://xiongod.github.io/halibote2/088.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -360,7 +360,7 @@
         "id": "4b43056262ac4c2dae16a9247c49d062"
     },
     {
-        "title": "89.《哈利·波特》第二部 第40集 康奈利·福吉1.m4a",
+        "title": "《哈利·波特》第二部 第集 康奈利·福吉.ma",
         "url": "https://xiongod.github.io/halibote2/089.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -369,7 +369,7 @@
         "id": "e3e7f3b53f88407697f0e40f7fc30067"
     },
     {
-        "title": "90.《哈利·波特》第二部 第41集 康奈利·福吉2.m4a",
+        "title": "《哈利·波特》第二部 第集 康奈利·福吉.ma",
         "url": "https://xiongod.github.io/halibote2/090.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -378,7 +378,7 @@
         "id": "0c855963c8c94401884ddf3113e502b0"
     },
     {
-        "title": "91.《哈利·波特》第二部 第42集 康奈利·福吉3.m4a",
+        "title": "《哈利·波特》第二部 第集 康奈利·福吉.ma",
         "url": "https://xiongod.github.io/halibote2/091.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -387,7 +387,7 @@
         "id": "5fc632637ad94a2cb4a67f6a2a53c3ac"
     },
     {
-        "title": "92.《哈利·波特》第二部 第43集 阿拉戈克1.m4a",
+        "title": "《哈利·波特》第二部 第集 阿拉戈克.ma",
         "url": "https://xiongod.github.io/halibote2/092.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -396,7 +396,7 @@
         "id": "6038db191b9e4637a0dfb26f93e145bb"
     },
     {
-        "title": "93.《哈利·波特》第二部 第44集 阿拉戈克2.m4a",
+        "title": "《哈利·波特》第二部 第集 阿拉戈克.ma",
         "url": "https://xiongod.github.io/halibote2/093.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -405,7 +405,7 @@
         "id": "bd617490cb3149189f0d6443fc08d009"
     },
     {
-        "title": "94.《哈利·波特》第二部 第45集 阿拉戈克3.m4a",
+        "title": "《哈利·波特》第二部 第集 阿拉戈克.ma",
         "url": "https://xiongod.github.io/halibote2/094.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -414,7 +414,7 @@
         "id": "52c06b9e2d20465f915846269c984e55"
     },
     {
-        "title": "95.《哈利·波特》第二部 第46集 密室1.m4a",
+        "title": "《哈利·波特》第二部 第集 密室.ma",
         "url": "https://xiongod.github.io/halibote2/095.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -423,7 +423,7 @@
         "id": "d2be2ede22384009a6a48a44edc2dd89"
     },
     {
-        "title": "96.《哈利·波特》第二部 第47集 密室2.m4a",
+        "title": "《哈利·波特》第二部 第集 密室.ma",
         "url": "https://xiongod.github.io/halibote2/096.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -432,7 +432,7 @@
         "id": "4736a5003f864d23bf38d07e036eed72"
     },
     {
-        "title": "97.《哈利·波特》第二部 第48集 密室3.m4a",
+        "title": "《哈利·波特》第二部 第集 密室.ma",
         "url": "https://xiongod.github.io/halibote2/097.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -441,7 +441,7 @@
         "id": "a26249e393864e34919b489d2ff1d0b5"
     },
     {
-        "title": "98.《哈利·波特》第二部 第49集 密室4.m4a",
+        "title": "《哈利·波特》第二部 第集 密室.ma",
         "url": "https://xiongod.github.io/halibote2/098.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -450,7 +450,7 @@
         "id": "b5bc713a205743559a64a03ac05dfe79"
     },
     {
-        "title": "99.《哈利·波特》第二部 第50集 斯莱特林的继承人1.m4a",
+        "title": "《哈利·波特》第二部 第集 斯莱特林的继承人.ma",
         "url": "https://xiongod.github.io/halibote2/099.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -459,7 +459,7 @@
         "id": "91ca329948cb4cd9a964b35f91276750"
     },
     {
-        "title": "100.《哈利·波特》第二部 第51集 斯莱特林的继承人2.m4a",
+        "title": "《哈利·波特》第二部 第集 斯莱特林的继承人.ma",
         "url": "https://xiongod.github.io/halibote2/100.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -468,7 +468,7 @@
         "id": "6f3c0e121fce4033a3f1ff159f49928d"
     },
     {
-        "title": "101.《哈利·波特》第二部 第52集 斯莱特林的继承人3.m4a",
+        "title": "《哈利·波特》第二部 第集 斯莱特林的继承人.ma",
         "url": "https://xiongod.github.io/halibote2/101.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -477,7 +477,7 @@
         "id": "cf3b948bd6ab446aaedd8c1c72c945ac"
     },
     {
-        "title": "102.《哈利·波特》第二部 第53集 斯莱特林的继承人4.m4a",
+        "title": "《哈利·波特》第二部 第集 斯莱特林的继承人.ma",
         "url": "https://xiongod.github.io/halibote2/102.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -486,7 +486,7 @@
         "id": "6003a586ade84fa4822af29bcc868708"
     },
     {
-        "title": "103.《哈利·波特》第二部 第54集 多比的报偿1.m4a",
+        "title": "《哈利·波特》第二部 第集 多比的报偿.ma",
         "url": "https://xiongod.github.io/halibote2/103.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -495,7 +495,7 @@
         "id": "995e22b693ed432bb193b4d215a6e703"
     },
     {
-        "title": "104.《哈利·波特》第二部 第55集 多比的报偿2.m4a",
+        "title": "《哈利·波特》第二部 第集 多比的报偿.ma",
         "url": "https://xiongod.github.io/halibote2/104.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -504,7 +504,7 @@
         "id": "8074617c6f5d461186d01f9a6202baae"
     },
     {
-        "title": "105.《哈利·波特》第二部 第56集 多比的报偿3.m4a",
+        "title": "《哈利·波特》第二部 第集 多比的报偿.ma",
         "url": "https://xiongod.github.io/halibote2/105.m4a",
         "duration": "",
         "published": "2024-05-21",
